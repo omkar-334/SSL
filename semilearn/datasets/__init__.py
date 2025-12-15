eb078600dc440bbe2d922f878ce9fc29965e8fde
Omkar Kabde <omkarkabde@gmail.com>
2025-12-15 14:04:21 +0530

    fix

--- semilearn/datasets/__init__.py
+++ semilearn/datasets/__init__.py
@@ -1,8 +1,46 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from semilearn.datasets.utils import split_ssl_data, get_collactor
-from semilearn.datasets.cv_datasets import get_cifar, get_eurosat, get_imagenet, get_medmnist, get_semi_aves, get_stl10, get_svhn, get_food101
-from semilearn.datasets.nlp_datasets import get_json_dset
-from semilearn.datasets.audio_datasets import get_pkl_dset
-from semilearn.datasets.samplers import name2sampler, DistributedSampler, WeightedDistributedSampler, ImageNetDistributedSampler
+from semilearn.datasets.audio_datasets import get_pkl_dset as get_pkl_dset
+from semilearn.datasets.cv_datasets import (
+    get_cifar as get_cifar,
+)
+from semilearn.datasets.cv_datasets import (
+    get_eurosat as get_eurosat,
+)
+from semilearn.datasets.cv_datasets import (
+    get_food101 as get_food101,
+)
+from semilearn.datasets.cv_datasets import (
+    get_imagenet as get_imagenet,
+)
+from semilearn.datasets.cv_datasets import (
+    get_medmnist as get_medmnist,
+)
+from semilearn.datasets.cv_datasets import (
+    get_semi_aves as get_semi_aves,
+)
+from semilearn.datasets.cv_datasets import (
+    get_stl10 as get_stl10,
+)
+from semilearn.datasets.cv_datasets import (
+    get_sugarcane as get_sugarcane,
+)
+from semilearn.datasets.cv_datasets import (
+    get_svhn as get_svhn,
+)
+from semilearn.datasets.nlp_datasets import get_json_dset as get_json_dset
+from semilearn.datasets.samplers import (
+    DistributedSampler as DistributedSampler,
+)
+from semilearn.datasets.samplers import (
+    ImageNetDistributedSampler as ImageNetDistributedSampler,
+)
+from semilearn.datasets.samplers import (
+    WeightedDistributedSampler as WeightedDistributedSampler,
+)
+from semilearn.datasets.samplers import (
+    name2sampler as name2sampler,
+)
+from semilearn.datasets.utils import get_collactor as get_collactor
+from semilearn.datasets.utils import split_ssl_data as split_ssl_data
